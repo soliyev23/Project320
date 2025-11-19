284fac0e926bb38c5a84ff940200f6b599ea6804
vedro666 <hahahajakajj@gmail.com>
2025-11-19 14:41:49 +0500

    Patch Ilyas

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ print(func_ilyas(3,4))
 
 from funcartur import artur
 print(artur(6,3))
+
+@app.get("/c2")
+def get_c2(x: float, y: float):
+    return {"result": c2(x, y)}
+@app.post("/c2")
+def post_c2(data: TwoNumbers):
+    return {"result": c2(data.x, data.y)}
